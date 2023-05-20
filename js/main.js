5dc2e4afb1e2106c5055b9d43190c7b5fee1ab41
bakakoshka <nastasemisenko@gmail.com>
2023-05-20 10:37:06 +0500

    готовый проект

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.fade-slide').slick({
+        arrows: true,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+    });
+});
